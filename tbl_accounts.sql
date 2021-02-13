1c581577d50a3d0ea2199caa6e2cc78bfc43e699
John Dexter Satsatin <58326502+johndexters1100@users.noreply.github.com>
2021-02-13 12:02:11 +0800

    Add files via upload

--- tbl_accounts.sql
+++ tbl_accounts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3308
--- Generation Time: Feb 12, 2021 at 10:43 AM
+-- Generation Time: Feb 13, 2021 at 04:02 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
 
@@ -34,9 +34,10 @@ CREATE TABLE IF NOT EXISTS `tbl_accounts` (
   `account_LastName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `account_FirstName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `account_MiddleName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `account_Section` varchar(50) NOT NULL,
   `account_Year` int(10) NOT NULL,
   `account_Email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `account_ContactNumber` int(11) NOT NULL,
+  `account_ContactNumber` varchar(20) NOT NULL,
   `account_Address` varchar(255) NOT NULL,
   `account_Birthday` date NOT NULL,
   `account_Age` int(10) NOT NULL,
@@ -48,8 +49,8 @@ CREATE TABLE IF NOT EXISTS `tbl_accounts` (
 -- Dumping data for table `tbl_accounts`
 --
 
-INSERT INTO `tbl_accounts` (`account_StudentNumber`, `account_LastName`, `account_FirstName`, `account_MiddleName`, `account_Year`, `account_Email`, `account_ContactNumber`, `account_Address`, `account_Birthday`, `account_Age`, `account_Password`) VALUES
-('2018-00058-BN-0', 'Satsatin', 'John Dexter', 'Ortiz', 3, 'johndexters1100@gmail.com', 2147483647, '9565 PASEO C.A. Ortiz St.', '2000-10-01', 20, 'pogiako');
+INSERT INTO `tbl_accounts` (`account_StudentNumber`, `account_LastName`, `account_FirstName`, `account_MiddleName`, `account_Section`, `account_Year`, `account_Email`, `account_ContactNumber`, `account_Address`, `account_Birthday`, `account_Age`, `account_Password`) VALUES
+('2018-00058-BN-0', 'Satsatin', 'John Dexter', 'Ortiz', 'BSIT', 3, 'johndexters1100@gmail.com', '09456515535', '9565 PASEO C.A. Ortiz St.', '2000-10-01', 20, 'pogiako');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
